fce26346dd955f51980d2b94e1db0828b176e0f7
rotemduvdevani <rotem.duvdevani@post.runi.ac.il>
2025-02-10 18:34:23 +0200

    now the url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Parking from "./pages/Parking";
 
 function App() {
     return (
         <BrowserRouter basename="/Ahuza">
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/parking" element={<Parking />} />
                 {/* Add other routes here */}
             </Routes>
         </BrowserRouter>
