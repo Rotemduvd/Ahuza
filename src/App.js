98ba81a7758928b5f91588ce8c246583d7cb9ed0
rotemduvdevani <rotem.duvdevani@post.runi.ac.il>
2025-02-10 18:30:32 +0200

    tried fixing the redirection home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Parking from "./pages/Parking";
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/parking" element={<Parking />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <BrowserRouter basename="/Ahuza">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* Add other routes here */}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
